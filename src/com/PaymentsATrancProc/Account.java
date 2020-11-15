612dc016d704982190ed0240141c56ff96a4eb67
barazi roi <barazi.roi@example.com>
2020-11-15 16:34:04 +0200

    init commit

---
+++ src/com/PaymentsATrancProc/Account.java
@@ -0,0 +1,10 @@
+package com.PaymentsATrancProc;
+
+public class Account {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/com/PaymentsATrancProc/StandardAccountOperations.java
@@ -0,0 +1,5 @@
+package com.PaymentsATrancProc;
+
+public enum StandardAccountOperations {
+
+}
