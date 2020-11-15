b3f88a0330c64cbc74425d328ff50c2d8c916cf2
barazi roi <barazi.roi@example.com>
2020-11-15 16:38:21 +0200

    new changes

--- src/com/PaymentsATrancProc/Account.java
+++ src/com/PaymentsATrancProc/Account.java
@@ -1,9 +1,25 @@
 package com.PaymentsATrancProc;
 
-public class Account {
+public interface Account { 
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		public void sendMoneyToAccount(Account accountTo, double moneyAmount) {
+		   
+		}
+		
+		public void withdrawMoney(double moneyAmount) {
+			
+		}
+		
+		public Transaction[] getTransactions() {
+			
+		}
+		
+		public static class Transaction {
+	        
+		}
 
 	}
 
