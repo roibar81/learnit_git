edba519d1defcffafc4109529566006dcf4dda08
barazi roi <barazi.roi@example.com>
2020-11-22 19:04:40 +0200

    test SSH connection

--- src/com/PaymentsATrancProc/Account.java
+++ src/com/PaymentsATrancProc/Account.java
@@ -1,6 +1,6 @@
 package com.PaymentsATrancProc;
 
-public interface Account { 
+public interface Account {                   
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
